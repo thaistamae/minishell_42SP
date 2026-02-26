dd29db81e69a731660013ab282884452fe2fc6da
Kaide Lucas Da Silva <kaidda-s@c1r5p3.42sp.org.br>
2026-02-26 20:01:55 -0300

    Inclusão de funções de error

---
+++ src/utils/error.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaidda-s <kaidda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 15:52:41 by kaidda-s          #+#    #+#             */
+/*   Updated: 2026/02/26 15:49:15 by kaidda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "minishell.h"
+
+int	error_command_not_found(char *args)
+{
+	if (args)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(args, 2);
+		ft_putstr_fd(": command not found\n", 2);
+	}
+	return (1);
+}
+
+int	error_fork()
+{
+	ft_putstr_fd("minishell: fork error\n", 2);
+	return (1);
+}
+
+void	free_array(char **array)
+{
+
+}
