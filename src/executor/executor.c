dd6f934a3824e14b6f6ffc415cfbc8f7fefffa64
Kaide Lucas Da Silva <kaidda-s@c1r5p3.42sp.org.br>
2026-02-26 20:03:34 -0300

    Funções principais do executor

---
+++ src/executor/executor.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaidda-s <kaidda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 22:09:10 by kaidda-s          #+#    #+#             */
+/*   Updated: 2026/02/25 18:39:20 by kaidda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "executor.h"
+
+//Valida se command existe
+int	execute_command(t_command *cmd, t_env *env)
+{
+	//Validação
+	if (!cmd || !cmd->args || !cmd->args[0])
+		return (1);
+	//Decide: Builtin ou Externo?
+	if (is_builtin(cmd->args[0]))
+		return  execute_builtin(cmd, env);
+	return execute_external(cmd, env);
+}
+
+//Função para comandos externos ex.: ls, mv, rm, mkdir...
+int	execute_external(t_command *cmd, t_env *env)
+{
+	pid_t	pid;
+	int		status;
+	char	*path;
+	
+	//Verificação de cmd
+	if (!cmd || !cmd->args || !cmd->args[0]) 
+		return (1);
+
+	//Encontrar executavel
+	path = find_executable(cmd->args[0], env);
+	if (!path)
+		return (error_command_not_found(cmd->args[0]));
+	//Cria processo filho
+	pid = fork();
+	if (pid < 0)
+		return (error_fork());
+	//No processo filho (pid == 0)
+	if (pid == 0)
+	{
+		execve(path, cmd->args, env->envp);
+		exit(1); //Se execve falhar
+	}
+	free(path);
+	waitpid(pid, &status, 0);
+
+	//Processar código de saída
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	return (1);
+}
+
