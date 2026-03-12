439678dea74d48be134487edec74b25ca59e0aee
Kaide Lucas Da Silva <kaidda-s@c1r3p4.42sp.org.br>
2026-03-11 21:48:30 -0300

    utils redir

--- src/executor/redir.utils.c
+++ src/executor/redir.utils.c
@@ -3,20 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   redir.utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaidda-s <kaidda-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: kaidda-s <kaidda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 20:17:05 by kaidda-s          #+#    #+#             */
-/*   Updated: 2026/03/11 01:56:16 by kaidda-s         ###   ########.fr       */
+/*   Updated: 2026/03/11 21:47:43 by kaidda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // Guardar os fds originais antes de redirecionar
-int save_std_fds(int *saved_stdin, int *saved_stdout);
+int save_std_fds(int *saved_stdin, int *saved_stdout)
+{
+	*saved_stdin = dup(0);
+	if (*saved_stdin == -1)
+		return (-1);
+	*saved_stdout = dup(1);
+	if (*saved_stdout == -1)
+		return (-1);
+	return (0);
+}
 
 // Voltar stdout/stdin aos valores originais
-void restore_std_fds(int saved_stdin, int saved_stdout);
+void restore_std_fds(int saved_stdin, int saved_stdout)
+{
+	saved_stdin = dup2(0);
+	close()
+	saved_stdout = dup2(1);
+	close()
+}
 
 // Abrir arquivo para escrita (output)
-int open_output_file(char *filename);
+int open_output_file(t_redir *redir)
+{
+	int fd;
+	int flags;
+
+	if (redir->type == T_REDIRECT_OUT)
+		flags = O_CREAT | O_WRONLY | O_TRUNC;
+	else if (redir->type == T_APPEND)
+		flags = O_CREAT | O_WRONLY | O_APPEND;
+	fd = open(redir->file,flags ,0644);
+	return (fd);
+}
