4df3312f87b72b2d0efce9f3268398d13c14deee
Kaide Lucas Da Silva <kaidda-s@c1r7p1.42sp.org.br>
2026-04-03 16:54:42 -0300

    Correção da impressão de argumentos

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,30 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaidda-s <kaidda-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: kaidda-s <kaidda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 10:48:02 by kaidda-s          #+#    #+#             */
-/*   Updated: 2026/03/22 13:46:59 by kaidda-s         ###   ########.fr       */
+/*   Updated: 2026/04/03 16:50:39 by kaidda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	is_n_flag(char *arg)
+{
+	int	i;
+
+	if (!arg || arg[0] != '-' || arg[1] == '\0')
+		return (0);
+	i = 1;
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	builtin_echo(t_command *cmd)
 {
 	int	i;
-	int	no_nexline;
+	int	no_newline;
 
 	i = 1;
-	no_nexline = 0;
-	if (cmd->args[i])
+	no_newline = 0;
+	while (cmd->args[i] && is_n_flag(cmd->args[i]))
+	{
+		no_newline = 1;
+		i++;
+	}
+	while (cmd->args[i])
 	{
 		ft_putstr_fd(cmd->args[i], STDOUT_FILENO);
 		if (cmd->args[i + 1])
 			ft_putstr_fd(" ", STDOUT_FILENO);
 		i++;
 	}
-	if (!no_nexline)
+	if (!no_newline)
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	return (0);
 }
